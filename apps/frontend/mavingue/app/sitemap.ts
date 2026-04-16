8bc200efe7a6da6af3e78f83d4a612bcd17b10af
CustCoding <custodiotitossetitosse@gmail.com>
2026-04-17 01:01:22 +0200

    feat(seo): add sitemap generation

---
+++ apps/frontend/mavingue/app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://estaleiromavingueonline.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://estaleiromavingueonline.com/public/catalogo",
+      lastModified: new Date(),
+    },
+  ];
+}
