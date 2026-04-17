61eaacf6fa743d96a66bb37404dbb8c4a3963594
CustCoding <custodiotitossetitosse@gmail.com>
2026-04-17 15:02:07 +0200

    fix(repository): adjust CustomerRepository queries/methods

--- apps/api/auth/app/src/main/java/com/custcoding/estaleiromavingue/App/repositories/CustomerRepository.java
+++ apps/api/auth/app/src/main/java/com/custcoding/estaleiromavingue/App/repositories/CustomerRepository.java
@@ -3,7 +3,10 @@ package com.custcoding.estaleiromavingue.App.repositories;
 import com.custcoding.estaleiromavingue.App.models.CustomerProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<CustomerProduct, Long> {
     boolean existsByEmail(String email);
     boolean existsByNuit(String nuit);
+    Optional<CustomerProduct> findByEmail(String email);
 }
