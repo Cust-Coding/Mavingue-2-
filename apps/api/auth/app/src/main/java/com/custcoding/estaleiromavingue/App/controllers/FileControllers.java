c9bd3b57c54c6f912984c830758275c93635d451
WesleyBule <bulewesley7@gmail.com>
2026-04-23 00:45:02 +0200

    fix: Imagem upload

--- apps/api/auth/app/src/main/java/com/custcoding/estaleiromavingue/App/controllers/FileControllers.java
+++ apps/api/auth/app/src/main/java/com/custcoding/estaleiromavingue/App/controllers/FileControllers.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -22,14 +23,29 @@ public class FileControllers {
     private final CloudinaryService cloudinary;
 
     @PostMapping("/upload")
-    public ResponseEntity<Map<String, String>> upload(@RequestParam("file") MultipartFile file) {
-        try{
+    public ResponseEntity<Map<String, Object>> upload(@RequestParam("file") MultipartFile file) {
+
+        Map<String, Object> response = new HashMap<>();
+        try {
             String folder = "products_upload";
             String url = cloudinary.uploadFile(file, folder);
-            return ResponseEntity.ok(Map.of("url", url));
+            response.put( "success", true);
+            response.put("message", "Upload realizado com sucesso");
+            response.put("url", url);
+
+            return ResponseEntity.ok(response);
+
+        }catch (IllegalArgumentException e) {
+                response.put("success", false);
+                response.put("message", e.getMessage());
+                return ResponseEntity.badRequest().body(response);
+
         }catch (IOException e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Map.of("error", "Falha no Upload: " + e.getMessage()));
+            response.put("message", "Falha no upload: " + e.getMessage());
+            response.put("success", false);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
+
         }
 
     }
